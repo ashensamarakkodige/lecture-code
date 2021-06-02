8e9e49b178b6827bf94c2c3ba8cf32bf4757e9a0
Patrick Pang <mail@patrickpang.net>
2021-06-02 16:45:48 +1000

    Added a1c.

---
+++ a1c.py
@@ -0,0 +1,23 @@
+shapes = [
+    {"type": "Square", "area": 150.5},
+    {"type": "Rectangle", "area": 80},
+    {"type": "Rectangle", "area": 660},
+    {"type": "Circle", "area": 68.2},
+    {"type": "Triangle", "area": 20}
+]
+
+
+class Shape:
+    def __init__(self, type, area):
+        self.type = type
+        self.area = area
+
+    def __str__(self):
+        return f"{self.type} with area size {self.area}"
+
+
+i = 1
+for data in shapes:
+    shape = Shape(data["type"], data["area"])
+    print(f"{i} - {shape}")
+    i += 1
